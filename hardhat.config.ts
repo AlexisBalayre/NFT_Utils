93cfa80f5feb2c21d31fa9eab37078b1600c0e0c
Alexis Balayre <60859013+AlexisBal@users.noreply.github.com>
2023-02-12 00:02:47 +0100

    Changed hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,11 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+require("dotenv").config();
+
+const PRIVATE_KEY_OWNER: string = process.env.PRIVATE_KEY_OWNER? process.env.PRIVATE_KEY_OWNER : "";
+const PRIVATE_KEY_MINTER: string = process.env.PRIVATE_KEY_MINTER? process.env.PRIVATE_KEY_MINTER : "";
+
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.17",
@@ -16,6 +21,18 @@ const config: HardhatUserConfig = {
       forking: {
         url: "https://polygon.llamarpc.com",
       }
+    }, 
+    polygon: {
+      url: process.env.RPC_URL_POLYGON,
+      accounts: [PRIVATE_KEY_OWNER, PRIVATE_KEY_MINTER]
+    },
+    bsc : {
+      url: process.env.RPC_URL_BSC,
+      accounts: [PRIVATE_KEY_OWNER, PRIVATE_KEY_MINTER]
+    },
+    optimism : {
+      url: process.env.RPC_URL_OPTIMISM,
+      accounts: [PRIVATE_KEY_OWNER, PRIVATE_KEY_MINTER]
     }
   }
 };
